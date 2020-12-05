fdd360329931f60623d76b4e2aea9a2440f69e8f
owencho <owencho200@gmail.com>
2020-12-05 23:10:08 +0800

    now added function to control pwm

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
  * Copyright (c) 2002-2005 STMicroelectronics
  */
 #include "stm8s.h"
-/*
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
 void GPIO_setup(void)
 {
 	GPIO_DeInit(GPIOC);
@@ -12,7 +15,7 @@ void GPIO_setup(void)
 						
 	GPIO_DeInit(GPIOD);
 	GPIO_Init(GPIOD,(GPIO_PIN_2 | GPIO_PIN_3|GPIO_PIN_4|
-						GPIO_PIN_5),GPIO_MODE_OUT_PP_HIGH_FAST);
+						GPIO_PIN_5|GPIO_PIN_6),GPIO_MODE_OUT_PP_HIGH_FAST);
 }
 void clock_setup(void)
 {
@@ -24,7 +27,7 @@ void clock_setup(void)
 	while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY) == FALSE);
 						
 	CLK_ClockSwitchCmd(ENABLE);
-	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV8);
+	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
 	CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
 						
 	CLK_ClockSwitchConfig(CLK_SWITCHMODE_AUTO, CLK_SOURCE_HSI, 
@@ -34,41 +37,80 @@ void clock_setup(void)
 	CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART1, ENABLE);
 	CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER1, ENABLE);
 }
-
+#define TIM1_PRESCALER_1 ((u16) 0x00)
 void TIM1_setup(void)
 {               
 	TIM1_DeInit();
 	//Init the period 
-	TIM1_TimeBaseInit(0,TIM1_COUNTERMODE_UP , 7999,0); 
-	/*1/frequency, count value 8000, frequency=16M/8000=2kHZ*/
-	/*
+
+	TIM1_TimeBaseInit(TIM1_PRESCALER_1,TIM1_COUNTERMODE_UP,799,0);
+	// 1/frequency, count value 800, frequency=16M/800=20kHZ
+	
 	TIM1_OC3Init(TIM1_OCMODE_PWM1, TIM1_OUTPUTSTATE_ENABLE, 
-								TIM1_OUTPUTNSTATE_ENABLE,2000, 
+								TIM1_OUTPUTNSTATE_ENABLE,0xFFFF, 
 								TIM1_OCPOLARITY_LOW, TIM1_OCNPOLARITY_HIGH,
 								TIM1_OCIDLESTATE_SET,
 								TIM1_OCNIDLESTATE_RESET);
 	/*4000/2000 * 100% = 50% duty cycle*/
-	/*							
+							
 	TIM1_Cmd(ENABLE);
 	TIM1_CtrlPWMOutputs(ENABLE);
 }
-/*
+
+void UART1_setup(void)
+{               
+	UART1_DeInit();
+
+	
+	UART1_Init(115200, UART1_WORDLENGTH_8D, 
+						UART1_STOPBITS_1 ,UART1_PARITY_NO, 
+						UART1_SYNCMODE_CLOCK_DISABLE , UART1_MODE_TXRX_ENABLE);
+
+							
+	UART1_Cmd(ENABLE);
+	TIM1_CtrlPWMOutputs(ENABLE);
+}
+
+void Serial_print_int (int number)  {
+	char count = 0;
+	char digit[5] = "";         
+	while (number != 0) //split the int to char array
+	{
+		digit[count] = number%10;
+		count++;
+		number = number/10;
+	}
+	while (count !=0) //print char array in correct direction
+	{
+		UART1_SendData8(digit[count-1] + 0x30);
+		while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET); 
+		count--;
+	}
+}
+
+
+void setLEDIntensity(uint16_t inputValue){
+	double calValue;
+	uint32_t value;
+	calValue = 800.00-((inputValue/255.00)*800.00);
+	value= (uint32_t)calValue;
+	TIM1_SetCompare3(value) ;
+	//Serial_print_int (value);
+}
+
+
 int main(void)
 {
 	GPIO_setup();
 	clock_setup();
 	TIM1_setup();
+	UART1_setup();
+	setLEDIntensity(200);
+	//TIM1_SetCompare3(value) ;
 	GPIO_WriteHigh(GPIOD,GPIO_PIN_4);
-	while (1);
+	while (1){
+		//Serial_print_int (value);
+	}
 }
 
-*/
 
-//#include "stm8s.h"
-
-main() {
-    
-	GPIO_Init(GPIOD, GPIO_PIN_ALL ,GPIO_MODE_OUT_PP_HIGH_SLOW);
-	GPIO_WriteHigh(GPIOD,GPIO_PIN_4);
-	while (1);
-}
