e276d51f60145aa20f606169e9f20f50c7739e21
owencho <owencho200@gmail.com>
2020-12-06 14:25:13 +0800

    added temperature, current and voltage sensor

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <math.h>
 
 void GPIO_setup(void)
 {
 	GPIO_DeInit(GPIOC);
-	GPIO_Init(GPIOC, (GPIO_PIN_4 | GPIO_PIN_3),
+	GPIO_Init(GPIOC, (GPIO_PIN_3),
 						GPIO_MODE_OUT_PP_HIGH_FAST);
+	GPIO_Init(GPIOC, (GPIO_PIN_4 ),
+						GPIO_MODE_IN_PU_NO_IT);
 						
 	GPIO_DeInit(GPIOD);
 	GPIO_Init(GPIOD,(GPIO_PIN_2 | GPIO_PIN_3|GPIO_PIN_4|
@@ -37,6 +40,7 @@ void clock_setup(void)
 	CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART1, ENABLE);
 	CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER1, ENABLE);
 }
+
 #define TIM1_PRESCALER_1 ((u16) 0x00)
 void TIM1_setup(void)
 {               
@@ -51,7 +55,6 @@ void TIM1_setup(void)
 								TIM1_OCPOLARITY_LOW, TIM1_OCNPOLARITY_HIGH,
 								TIM1_OCIDLESTATE_SET,
 								TIM1_OCNIDLESTATE_RESET);
-	/*4000/2000 * 100% = 50% duty cycle*/
 							
 	TIM1_Cmd(ENABLE);
 	TIM1_CtrlPWMOutputs(ENABLE);
@@ -68,7 +71,21 @@ void UART1_setup(void)
 
 							
 	UART1_Cmd(ENABLE);
-	TIM1_CtrlPWMOutputs(ENABLE);
+}
+
+void ADC1_setup(void)
+{               
+	ADC1_DeInit();
+	ADC1_Init(ADC1_CONVERSIONMODE_CONTINUOUS,  
+					ADC1_CHANNEL_2,  
+					ADC1_PRESSEL_FCPU_D2 ,  
+					ADC1_EXTTRIG_GPIO,  
+					DISABLE,  
+					ADC1_ALIGN_RIGHT ,  
+					ADC1_SCHMITTTRIG_ALL ,  
+					DISABLE); 
+	
+	ADC1_Cmd(ENABLE);
 }
 
 void Serial_print_int (int number)  {
@@ -87,7 +104,51 @@ void Serial_print_int (int number)  {
 		count--;
 	}
 }
+double getTemperature(void){
+	uint16_t adcValue;
+	double denomOfTempEquation;
+	ADC1_ConversionConfig  ( 	ADC1_CONVERSIONMODE_SINGLE ,  
+														ADC1_CHANNEL_2 ,  
+														ADC1_ALIGN_RIGHT); 
+	ADC1_StartConversion();
+	while(ADC1_GetFlagStatus(ADC1_FLAG_EOC) == FALSE);
+	adcValue = ADC1_GetConversionValue();
+	denomOfTempEquation = (1/337.3)+(1/3950.00)
+												*log((1023.00/adcValue)-1);
+	
+	//Serial_print_int (adcValue);
+	ADC1_ClearFlag(ADC1_FLAG_EOC);
+	return (1/denomOfTempEquation)-273.15;
+	//return adcValue;
+}
 
+double getVoltage(void){
+	uint16_t adcValue;
+	ADC1_ConversionConfig  ( 	ADC1_CONVERSIONMODE_SINGLE ,  
+														ADC1_CHANNEL_4 ,  
+														ADC1_ALIGN_RIGHT); 
+	ADC1_StartConversion();
+	while(ADC1_GetFlagStatus(ADC1_FLAG_EOC) == FALSE);
+	adcValue = ADC1_GetConversionValue();
+	ADC1_ClearFlag(ADC1_FLAG_EOC);
+	return (adcValue/1023.00)*5;
+}
+#define SENSE_RESISTOR 0.01096
+#define OP_AMP_GAIN 168.98
+
+double getCurrent(void){
+	uint16_t adcValue;
+	uint16_t voltageADC;
+	ADC1_ConversionConfig  ( 	ADC1_CONVERSIONMODE_SINGLE ,  
+														ADC1_CHANNEL_3 ,  
+														ADC1_ALIGN_RIGHT); 
+	ADC1_StartConversion();
+	while(ADC1_GetFlagStatus(ADC1_FLAG_EOC) == FALSE);
+	adcValue = ADC1_GetConversionValue();
+	voltageADC = (adcValue/1023.00)*5;
+	ADC1_ClearFlag(ADC1_FLAG_EOC);
+	return voltageADC/(OP_AMP_GAIN*SENSE_RESISTOR);
+}
 
 void setLEDIntensity(uint16_t inputValue){
 	double calValue;
@@ -98,14 +159,20 @@ void setLEDIntensity(uint16_t inputValue){
 	//Serial_print_int (value);
 }
 
-
+double testTempValue;
+double testVoltageValue;
+double testCurrentValue;
 int main(void)
 {
 	GPIO_setup();
 	clock_setup();
 	TIM1_setup();
 	UART1_setup();
+	ADC1_setup();
 	setLEDIntensity(200);
+	testTempValue = getTemperature();
+	testVoltageValue = getVoltage();
+	testCurrentValue = getCurrent();
 	//TIM1_SetCompare3(value) ;
 	GPIO_WriteHigh(GPIOD,GPIO_PIN_4);
 	while (1){
